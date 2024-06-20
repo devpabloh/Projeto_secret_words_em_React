7ca2f1b29058551142e1acde0b8012a5f20231d5
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-20 15:44:02 -0300

    atualização

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 /* React */
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 /* Importação dos dados - data */
 import { wordsList } from './data/word';
@@ -16,67 +16,60 @@ const stages = [
     {id: 1, name: "start"},
     {id: 2, name: "game"},
     {id: 3, name: "end"},
-]
+];
 
 function App() {
 
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  const [pickedWord, setPickedWord]= useState("");
-  const [pickedCategory, setPickedCategory] = useState("")
-  const [letters, setLetters] = useState([])
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
 
-  const pickedWordAndCategory = ()=>{
-    //pick a random category
-    const categories = Object.keys(words)
-    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+  const pickedWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * categories.length)];
 
-    //pick a random word
+    // pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)];
 
-    return {word, category} = pickedWordAndCategory();
-
+    return { word, category };
   };
 
-
   /* starts the secret word game */
-  const startGame = ()=>{
-
-    const {word, category} = pickedWordAndCategory()
+  const startGame = () => {
+    const { word, category } = pickedWordAndCategory();
 
     /* picked word and picked category */
-    pickedWordAndCategory();
 
     /* Create an array of letters */
     let wordLetters = word.split("");
-
     wordLetters = wordLetters.map((l) => l.toLowerCase());
 
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
 
-    setPickedWord(word)
-    setPickedCategory(letters)
+    setGameStage(stages[1].name);
+  };
 
-    setGameStage(stages[1].name)
-  }
-
-  /* process the latter input */
-
-  const verifyLetter = ()=>{
-    setGameStage(stages[2].name)
+  /* process the letter input */
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
   };
 
   /* Restart the game */
-
-  const retry = ()=>{
-    setGameStage(stages[0].name)
-  }
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen startGame = {startGame} />}
-      {gameStage === "game" && <Game verifyLetter = {verifyLetter}/>}
-      {gameStage === "end" && <GameOver retry = {retry}/>}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
