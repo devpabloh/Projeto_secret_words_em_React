b304092eb154cf319408517b9a599c307118d170
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-20 11:25:14 -0300

    atualização

---
+++ secretword/src/data/word.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    carro: ["Banana", "Maça", "pêra", "Mamão", "Laranja"],
+    carro: ["Braço", "Perna", "Cérebro", "Prescoço", "Olhos"],
+    carro: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    carro: ["Linguagem", "FrameWork", "JavaScript", "React"],
+    carro: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
+
